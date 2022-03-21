4f910b6523d61aa3d12fa01a6a8a49822873c8d7
kai <iftikharasjad@gmail.com>
2022-03-21 13:49:07 +0500

    fixing serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,8 +3,6 @@ from .models import Customer
 
 
 class CustomerSerializer(serializers.ModelSerializer):
-    user_id = serializers.CharField()
-
     class Meta:
         model = Customer
-        fields = ['id', 'user_id', 'brand_name', 'business_category', 'phone', 'website_url', 'address']
+        fields = ['id', 'user', 'brand_name', 'business_category', 'phone', 'website_url', 'address']
