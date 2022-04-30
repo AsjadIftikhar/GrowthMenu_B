f0784c75928f93fb828e3f4d98d492a282714949
kai <iftikharasjad@gmail.com>
2022-04-30 20:59:58 +0500

    order api

--- api/views/order.py
+++ api/views/order.py
@@ -4,8 +4,18 @@ from rest_framework.decorators import action, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
+from profiles.models import Customer
 
 
-class OrderViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
+class OrderViewSet(ModelViewSet):
+    # permission_classes = [IsAuthenticated]
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    filter_backends = [DjangoFilterBackend]
+
+    # def get_queryset(self):
+    #     queryset = Order.objects.filter(customer__user=self.request.user.id)
+    #     print(self.request.user)
+    #     return queryset
