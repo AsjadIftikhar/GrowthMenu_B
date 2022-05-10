69c88a96a389826c80fab410045d6741bc0b056e
Ali Iqbal <ali.iqbal028@gmail.com>
2022-05-10 18:27:58 +0500

    order list of the logged in user

--- GrowthMenu_B/settings.py
+++ GrowthMenu_B/settings.py
@@ -27,11 +27,17 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOWED_ORIGINS = [
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+#     'http://127.0.0.1:3000',
+# ]
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
-    'http://127.0.0.1:3000',
+    'http://localhost:8000',
+    'http://localhost:8080',
 ]
-
 # Application definition
 
 INSTALLED_APPS = [
